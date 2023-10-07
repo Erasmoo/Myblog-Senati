61aa257b12a42aef0a15969c48c398af3b374420
Jorge Erasmo <erasmojorge274@gmail.com>
2023-10-07 08:37:55 -0500

    mi primer commit

---
+++ GAAA/index.php
@@ -0,0 +1,38 @@
+<!php>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="index,follow">
+	<meta name="description" content="hola mundo,iniciando con html, html desde cero">
+	<meta name="keywords" content="mundo, básico, cero,html,html5">
+	<meta name="author" content="roberto, robertito, bc">
+	<link rel="stylesheet" type="text/css" href="css/estyle.css">
+	<title>HOLA ERASMO</title>
+</head>
+<body>
+</head>
+<body> 
+	<header class="container">
+		<nav class="menu">
+			<ul>
+				<li>
+					<a href="">PRINCIPAL</a>
+				</li>
+				<li>
+					<a href="">LISTADO DE CLIENTES</a>
+				</li>
+				<li>
+					<a href="">REGISTRO DE CLIENTES</a>
+				</li>
+				<li>
+					<a href="">ACTUALIZACION DE DATOS</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+</body>
+</html>
